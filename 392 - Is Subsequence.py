cd12e8b90480c761646be17619e9373dcb3578a5
praveen-ah <ahpraveen01@gmail.com>
2025-08-23 21:46:04 +0530

    initial commit

---
+++ 392 - Is Subsequence.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        return len(s) == i
+
+
+s = "abc"
+t = "ahbgdc"
+obj = Solution()
+print(obj.isSubsequence(s, t))
+'''
+Example 1:
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Example 2:
+Input: s = "axc", t = "ahbgdc"
+Output: false
+'''
